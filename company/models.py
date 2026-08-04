1f779f5af3b54901c6454000aed9b651707b82df
kalhara weragoda <kalharaweragoda@gmail.com>
2026-08-04 14:06:48 +0530

    code company models.py

--- company/models.py
+++ company/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from user.models import User
 
-# Create your models here.
+
+class Company(models.Model):
+    employer = models.OneToOneField(User, on_delete=models.CASCADE, related_name='company')
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    website = models.URLField(blank=True)
+    location = models.CharField(max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
