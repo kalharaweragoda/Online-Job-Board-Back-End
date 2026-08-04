bd2cf68963f4cb9d17d94d58e7d10d17cfe91ec9
kalhara weragoda <kalharaweragoda@gmail.com>
2026-08-04 17:21:13 +0530

    create  setting.py

--- online_job_board_backend/settings.py
+++ online_job_board_backend/settings.py
@@ -37,7 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'rest_framework',
+    'user',
+    'company',
+    'category',
+    'job',
+    'application',
+]   
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -79,6 +85,8 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'user.User'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
@@ -116,18 +124,3 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'user',
-    'company',
-    'category',
-    'job',
-    'application',
-]   
