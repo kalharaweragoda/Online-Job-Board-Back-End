cdd76df7d91337e15b26b7d0b6d8374481b3124c
kalhara weragoda <kalharaweragoda@gmail.com>
2026-08-05 15:20:08 +0530

    create category views.py

--- category/views.py
+++ category/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics, permissions
+from .models import Category
+from .serializers import CategorySerializer
+
+
+class CategoryListView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [permissions.AllowAny()]   # anyone can browse categories
+        return [permissions.IsAuthenticated()]
