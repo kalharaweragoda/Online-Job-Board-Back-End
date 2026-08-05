1b78a5032b5866a736414f1be818a8377f3e525a
kalhara weragoda <kalharaweragoda@gmail.com>
2026-08-05 13:17:12 +0530

    create user permissions.py

---
+++ user/permissions.py
@@ -0,0 +1,10 @@
+from rest_framework.permissions import BasePermission
+
+class IsEmployer(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == 'employer'
+
+
+class IsJobSeeker(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == 'jobseeker'
