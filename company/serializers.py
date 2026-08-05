6d55eb735ee4ff68d505d6fb95be3973ba6edad7
kalhara weragoda <kalharaweragoda@gmail.com>
2026-08-05 13:06:25 +0530

    create company serializers.py

---
+++ company/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Company
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = ['id', 'employer', 'name', 'description', 'website', 'location', 'created_at']
+        read_only_fields = ['employer']
