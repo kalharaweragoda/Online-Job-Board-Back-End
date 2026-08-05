f06fdbc74a89e83f86152bbf8ab85a012039b6e2
kalhara weragoda <kalharaweragoda@gmail.com>
2026-08-05 13:01:39 +0530

    create user serializers.py

---
+++ user/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+
+from .models import User
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password', 'role', 'phone']
+
+    def create(self, validated_data):
+        user = User(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            role=validated_data['role'],
+            phone=validated_data.get('phone', ''),
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'role', 'phone']
