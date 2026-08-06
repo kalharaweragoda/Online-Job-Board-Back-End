04117b48d088b29fe06afab46e0cdb0b524e31ac
kalhara weragoda <kalharaweragoda@gmail.com>
2026-08-06 09:14:36 +0530

    create company urls.path

--- online_job_board_backend/urls.py
+++ online_job_board_backend/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('user.urls')),
+    path('api/companies/', include('company.urls')),
 ]
